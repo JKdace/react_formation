6843d4fdd73299d8621093462ce988ef8553c182
JKdace <julienbachorz@mac.com>
2023-09-11 12:16:02 +0100

    nouvel app

---
+++ src/App/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button from './components/Button/Button.jsx';
+/**
+ * 
+ * @param {*} props 
+ * @returns 
+ */
+const App=(props) => {
+    return <div className='App'>
+        HW<hr/>
+        <Button text="bob"/>
+        <Button text="patrick"/>
+        </div>
+};
+export default App;
