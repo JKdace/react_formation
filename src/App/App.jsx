fc6b2448c8a5e9f889b47fee1a57228e8ce07d5d
JKdace <julienbachorz@mac.com>
2023-09-12 16:38:04 +0100

    gestion liste images

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import FlexH3G from "./components/layout/FlexH3G/FlexH3G"
 import FlexW1G from "./components/layout/FlexW1G/FlexW1G"
 import Header from "./components/ui/Header/Header"
@@ -10,14 +10,13 @@ import { DummyMeme } from './interfaces/common'
 
 const App = () => {
   const [current, setCurrent] = useState(DummyMeme)
-  const [images, setImages] = useState([
-    {
-      id: 0,
-      w: 1200,
-      h: 1000,
-      url: "https://www.imagespourtoi.com/lesimages/bob-eponge/image-bob-eponge-2.jpg"
-    }
-  ])
+  const [images, setImages] = useState([])
+  useEffect(() => {
+    fetch('http://localhost:7956/images')
+    .then(response => response.json())
+    .then(a => setImages(a) )
+  }, [])
+  
   return (
     <div className='App'>
       <FlexH3G>
@@ -25,7 +24,7 @@ const App = () => {
         <Navbar></Navbar>
         <FlexW1G>
           <MemeViewer meme={current} image={images.find((img) => img.id === current.imageId)} basePath=""></MemeViewer>
-          <MemeForm meme={current} onMemeChange={(newMeme) => {
+          <MemeForm meme={current} images={images} onMemeChange={(newMeme) => {
               setCurrent(newMeme);
             }}></MemeForm>
         </FlexW1G>
