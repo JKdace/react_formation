7998d9a63379b2d6bcd82dcf73cfd12e350a2323
JKdace <julienbachorz@mac.com>
2023-09-12 10:20:46 +0100

    configuration generateur

---
+++ grc_templates/functionWithState/TemplateName.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './TemplateName.module.css';
+
+const templateNameInitialState = {}
+const TemplateName = (props) => {
+  useEffect(() => {
+    console.log('montage de TemplateName')
+  }, [])
+  const [state, setState] = useState(templateNameInitialState)
+  return (
+    <div className={styles.TemplateName} data-testid="TemplateName">
+      TemplateName Component
+    </div>)
+};
+
+TemplateName.propTypes = {};
+
+TemplateName.defaultProps = {};
+
+export default TemplateName;
