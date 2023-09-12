a727e0684a7b2c51adb3c56db66713274d907f92
JKdace <julienbachorz@mac.com>
2023-09-12 10:21:21 +0100

    initialisation des composants

---
+++ src/App/components/MemeForm/MemeForm.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './MemeForm.module.css';
+
+const memeFormInitialState = {}
+const MemeForm = (props) => {
+  useEffect(() => {
+    console.log('montage de MemeForm')
+  }, [])
+  const [state, setState] = useState(memeFormInitialState)
+  return (
+    <div className={styles.MemeForm} data-testid="MemeForm">
+      MemeForm Component
+    </div>)
+};
+
+MemeForm.propTypes = {};
+
+MemeForm.defaultProps = {};
+
+export default MemeForm;
---
+++ src/App/components/layout/FlexW1G/FlexW1G.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styles from "./FlexW1G.module.css"
+
+const FlexW1G = (props) => {
+  return (
+    <div className={styles.FlexW1G} data-testid="FlexW1G">
+        {props.children}
+    </div>
+    );
+};
+
+FlexW1G.propTypes = {
+    children: PropTypes.any.isRequired
+};
+
+FlexW1G.defaultProps = {
+    children: <div>Template layout empty component</div>
+};
+
+export default FlexW1G;
