ef1069e5d7bb6df11513199fc41005e74929fb84
JKdace <julienbachorz@mac.com>
2023-09-13 11:35:30 +0100

    extrareducers async pour faire du rest

--- src/App/store/ressources.js
+++ src/App/store/ressources.js
@@ -17,14 +17,38 @@ const ressources = createSlice({
             state.images.splice(0);
             state.images.push(...action.payload);
         }
+    },
+    // extrareducers pour gestion asynchrone et écoute d'autres états pour y réagir
+    extraReducers(builder){
+        builder.addCase('ressources/fetchAllRessources/fulfilled',(state,action)=>{
+            state.images.splice(0);
+            state.images.push(...action.payload.images);
+            state.memes.splice(0);
+            state.memes.push(...action.payload.memes);
+        });
+        /* builder.addCase('ressources/fetchMemes/fulfilled',(state,action)=>{
+            state.memes.splice(0);
+            state.memes.push(...action.payload);
+        }); */
     }
 });
 
 export const fetchAllRessources = createAsyncThunk('ressources/fetchAllRessources',async ()=>{
-    const pimages = await fetch(REST_URL + 'images');
-    const images = await pimages.json();
-    return images;
+    const a = await Promise.all([fetch(REST_URL + 'images'), fetch(REST_URL + 'memes')]);
+    const images = await a[0].json();
+    const memes = await a[1].json();
+    
+    /* const pimages = await fetch(REST_URL + 'images');
+    const images = await pimages.json(); */
+    return {images:images, memes:memes};
 })
+
+// export const fetchMemes = createAsyncThunk('ressources/fetchMemes',async ()=>{
+//     const pmemes = await fetch(REST_URL + 'memes');
+//     const memes = await pmemes.json();
+//     return memes;
+// })
+
 
 export const {addImage,loadImagesFromArg} = ressources.actions
 
