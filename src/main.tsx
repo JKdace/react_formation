365cef239f9c3663146ed5367269116438a3f347
JKdace <julienbachorz@mac.com>
2023-09-13 17:31:11 +0100

    ajout router

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App/App.jsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import {store} from './App/store/store'
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App/App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./App/store/store";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
