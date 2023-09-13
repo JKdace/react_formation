fa1d0f4c0aefc3fb757f98a1f7b7f91a5c3f00ff
JKdace <julienbachorz@mac.com>
2023-09-13 17:30:08 +0100

    chargt imgs + memes + écoute sauvegarde

--- src/App/store/ressources.js
+++ src/App/store/ressources.js
@@ -2,46 +2,62 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { REST_URL } from "../config/constantes";
 
 const initialState = {
-    memes:[],
-    images:[]
+  memes: [],
+  images: [],
 };
 
 const ressources = createSlice({
-    name: 'ressources',
-    initialState,
-    reducers: {
-        addImage(state,action){
-            state.images.push(action.payload);
-        },
-        loadImagesFromArg(state,action){
-            state.images.splice(0);
-            state.images.push(...action.payload);
-        }
+  name: "ressources",
+  initialState,
+  reducers: {
+    addImage(state, action) {
+      state.images.push(action.payload);
     },
-    // extrareducers pour gestion asynchrone et écoute d'autres états pour y réagir
-    extraReducers(builder){
-        builder.addCase('ressources/fetchAllRessources/fulfilled',(state,action)=>{
-            state.images.splice(0);
-            state.images.push(...action.payload.images);
-            state.memes.splice(0);
-            state.memes.push(...action.payload.memes);
-        });
-        /* builder.addCase('ressources/fetchMemes/fulfilled',(state,action)=>{
+    loadImagesFromArg(state, action) {
+      state.images.splice(0);
+      state.images.push(...action.payload);
+    },
+  },
+  // extrareducers pour gestion asynchrone et écoute d'autres états pour y réagir
+  extraReducers(builder) {
+    builder.addCase(
+      "ressources/fetchAllRessources/fulfilled",
+      (state, action) => {
+        state.images.splice(0);
+        state.images.push(...action.payload.images);
+        state.memes.splice(0);
+        state.memes.push(...action.payload.memes);
+      }
+    );
+    // On écouter le submit pour mettre à jour le thumbnail sans avoir à recharger la page
+    builder.addCase("current/save/fulfilled", (state, action) => {
+      const position = state.memes.findIndex((m) => m.id === action.payload.id);
+      if (position !== -1) state.memes[position] = action.payload;
+      else state.memes.push(action.payload);
+    });
+
+    /* builder.addCase('ressources/fetchMemes/fulfilled',(state,action)=>{
             state.memes.splice(0);
             state.memes.push(...action.payload);
         }); */
-    }
+  },
 });
 
-export const fetchAllRessources = createAsyncThunk('ressources/fetchAllRessources',async ()=>{
-    const a = await Promise.all([fetch(REST_URL + 'images'), fetch(REST_URL + 'memes')]);
+export const fetchAllRessources = createAsyncThunk(
+  "ressources/fetchAllRessources",
+  async () => {
+    const a = await Promise.all([
+      fetch(REST_URL + "images"),
+      fetch(REST_URL + "memes"),
+    ]);
     const images = await a[0].json();
     const memes = await a[1].json();
-    
+
     /* const pimages = await fetch(REST_URL + 'images');
     const images = await pimages.json(); */
-    return {images:images, memes:memes};
-})
+    return { images: images, memes: memes };
+  }
+);
 
 // export const fetchMemes = createAsyncThunk('ressources/fetchMemes',async ()=>{
 //     const pmemes = await fetch(REST_URL + 'memes');
@@ -49,7 +65,6 @@ export const fetchAllRessources = createAsyncThunk('ressources/fetchAllRessource
 //     return memes;
 // })
 
+export const { addImage, loadImagesFromArg } = ressources.actions;
 
-export const {addImage,loadImagesFromArg} = ressources.actions
-
-export default ressources.reducer
+export default ressources.reducer;
